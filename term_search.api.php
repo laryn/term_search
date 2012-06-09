4fea91691d63cc50ab330a4d2bb43de7b63ba197
unknown <Scott@MININT-U3VUM50.(none)>
2012-06-09 13:44:12 -0400

    Ventral Changes

--- term_search.api.php
+++ term_search.api.php
@@ -61,7 +61,7 @@ function hook_term_update_index($term) {
  * @param $term
  *   The term object being display in a search result.
  *
- * @return
+ * @return 
  *   A string (can include html) that will be added to the rendered search
  *   result for this term.
  */
@@ -81,4 +81,3 @@ function hook_term_search_result($term) {
   }
   return $text;
 }
-
