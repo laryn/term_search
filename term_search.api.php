63b75253ff9312fc615ba5f303c62ae55b4296d3
laryn <laryn@cedc.org>
2019-06-04 14:31:02 -0500

    #1 vid -> vocabulary WIP and cleanup

--- term_search.api.php
+++ term_search.api.php
@@ -3,7 +3,7 @@
  * @file
  * This file contains no working PHP code; it exists to provide additional
  * documentation for doxygen as well as to document hooks in the standard
- * Drupal manner.
+ * Backdrop manner.
  */
 
 /**
